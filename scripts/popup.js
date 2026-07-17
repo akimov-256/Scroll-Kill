576713b1d2607f66398243c9e4c1afba6c0eba4f
Yovsky <mouloudguenane651@gmail.com>
2026-07-17 22:00:04 +0100

    feat: Added preservation of ui switch state when opening/closing the extension popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,8 +1,13 @@
 let uiSwitch = document.getElementById("checkBox");
 
+chrome.storage.local.get("active", (value) => {
+    console.log(value.active);
+    uiSwitch.checked = value.active !== false; 
+});
+
 uiSwitch.addEventListener('change', (event) => {
     if (event.target.checked)
-        console.log('extension is activated');
+        chrome.storage.local.set({active: true});
     else
-        console.log('extension is deactivated');
+        chrome.storage.local.set({active: false});
 });
